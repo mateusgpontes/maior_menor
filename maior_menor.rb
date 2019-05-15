93498c67433048a2dbfa65a698a019b2bf399122
mateusgpontes <mateus.gpontes@hotmail.com>
2019-05-15 13:32:49 -0300

    add logic check number

--- maior_menor.rb
+++ maior_menor.rb
@@ -6,20 +6,37 @@ puts "Qual seu nome?"
 nome = gets
 
 #Pula 2 linha
-puts 
-puts 
+puts "\n\n"
 
 #Informa que irá começar o jogo
 puts "Que comecem os jogos, #{nome}" 
 puts "Escolha um numero de 0 a 200..."
-
+puts "\n"
+ 
 #Escolhe o número
 secret_number = 165
-puts "Escolhido... Adivinhe o número secreto."
-puts 
-puts "Primeira Tentativa"
-puts "Coloque seu numero"
-chute = gets
-puts
-puts "Acertou? Você chutou #{chute}"
-puts chute.to_i == secret_number
+tentativas = 0
+
+puts "Adivinhe o número secreto."
+puts "\n\n"
+
+for tentativa in 1..3
+    puts "Tentatica #{tentativa} de 3"
+    puts "Coloque seu numero"
+
+    chute = gets
+    #Verifica número
+
+    verifica = chute.to_i == secret_number
+    maior = chute.to_i > secret_number
+
+    if verifica
+        puts "Você Acertou!"
+    elsif maior
+        puts "Você Errou! seu chute foi #{chute} o número secreto é menor"
+    else
+        puts "Você Errou! seu chute foi #{chute} o número secreto é maior"
+    end
+
+    puts "\n\n"
+end
